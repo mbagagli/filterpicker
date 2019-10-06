ce05cdb61c5b3f13d1953e92ab47a8332b41b298
matteo <matteo@bagagli.ethz.ch>
2019-10-06 18:33:22 +0200

    patches added, examples added. ready to merge in master

--- filterpicker/cli/fp_script.py
+++ filterpicker/cli/fp_script.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 import numpy as np
-
 from filterpicker import filterpicker as FP
 
-
 def main():
-    fpp = FP.FilterPicker(0.008, np.loadtxt('./tests/fg_sac.npa'),
+    fpp = FP.FilterPicker(0.008, np.loadtxt('../../tests/fg_sac.npa/tests/fg_sac.npa'),
                           filter_window=1.6, longterm_window=3.2, t_up=0.16,
                           threshold_1=20, threshold_2=10)
     pidx, punc, pfrq = fpp.run()
